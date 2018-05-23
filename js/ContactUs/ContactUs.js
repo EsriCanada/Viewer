b005a6d94a358c79278da7f24f9f377595cdef31
Horia Tudosie <htudosie@esri.ca>
2018-05-23 15:38:58 -0400

    Added ContactUs prototype

---
+++ js/ContactUs/ContactUs.js
@@ -0,0 +1,49 @@
+define([
+    "dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "dojo/dom","esri/kernel",
+    "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/on",
+    "dojo/query", "dijit/registry",
+    "dojo/text!application/ContactUs/Templates/ContactUs.html",
+    // "dojo/i18n!application/nls/ContactUs",
+    "dojo/dom-class", "dojo/dom-attr", "dojo/dom-style",
+    "dojo/dom-construct", "dojo/_base/event", "esri/lang",
+    "dojo/NodeList-dom", "dojo/NodeList-traverse"
+
+    ], function (
+        Evented, declare, _lang, has, dom, esriNS,
+        _WidgetBase, _TemplatedMixin, on,
+        query, registry,
+        ContactUsTemplate, 
+        // i18n,
+        domClass, domAttr, domStyle,
+        domConstruct, event, esriLang
+    ) {
+    var Widget = declare("esri.dijit.ContactUs", [_WidgetBase, _TemplatedMixin, Evented], {
+        templateString: ContactUsTemplate,
+
+        options: {
+            emailAddress: 'example@email.com',
+            sunject: 'Email Subject', 
+            body: 'Email Body Text'
+        },
+
+        constructor: function (options, srcRefNode) {
+            this.defaults = _lang.mixin({}, this.options, options);
+            //this._i18n = i18n;
+            this.domNode = srcRefNode;
+
+            var link = document.createElement("link");
+            link.href = "js/ContactUs/Templates/ContactUs.css";
+            link.type = "text/css";
+            link.rel = "stylesheet";
+            query('head')[0].appendChild(link);
+        },
+
+        startup: function () {
+        }
+    });
+
+    if (has("extend-esri")) {
+        _lang.setObject("dijit.ContactUs", Widget, esriNS);
+    }
+    return Widget;
+});
