dd71fe56a876d7a3c0394391395f9903a27fed95
Horia Tudosie <htudosie@esri.ca>
2018-07-11 18:29:43 -0400

    Cleaup format

--- js/utils.js
+++ js/utils.js
@@ -1,22 +1,21 @@
 "use strict"
 
-function isIE11()
-{
+function isIE11() {
     return (
         (navigator.appName == 'Microsoft Internet Explorer') ||
         (
             (navigator.appName == 'Netscape') &&
             (new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null))
-        );
+    );
 }
 
 function isChrome() {
-    return ((navigator.userAgent.toLowerCase().indexOf('chrome') > -1) &&(navigator.vendor.toLowerCase().indexOf("google inc.") > -1));
+    return ((navigator.userAgent.toLowerCase().indexOf('chrome') > -1) && (navigator.vendor.toLowerCase().indexOf("google inc.") > -1));
 }
 
 if (!Element.prototype.matches)
     Element.prototype.matches = Element.prototype.msMatchesSelector ||
-                                Element.prototype.webkitMatchesSelector;
+    Element.prototype.webkitMatchesSelector;
 
 if (!Element.prototype.closest)
     Element.prototype.closest = function(s) {
@@ -34,58 +33,57 @@ if (!Element.prototype.closest)
         return null;
     };
 
-Number.prototype.padLeft = function (n,str){
-    return new Array(n-String(this).length+1).join(str||'0')+this;
+Number.prototype.padLeft = function(n, str) {
+    return new Array(n - String(this).length + 1).join(str || '0') + this;
 };
 
 Date.prototype.getSQLDate = function() {
-    if(this.toDateString() === "Invalid Date") {
+    if (this.toDateString() === "Invalid Date") {
         return null;
     }
-    return this.getFullYear().padLeft(4)+(this.getMonth()+1).padLeft(2)+this.getDate().padLeft(2);
+    return this.getFullYear().padLeft(4) + (this.getMonth() + 1).padLeft(2) + this.getDate().padLeft(2);
 };
 
 
 // Add ECMA262-5 method binding if not supported natively
 //
 if (!('bind' in Function.prototype)) {
-    Function.prototype.bind= function(owner) {
-        var that= this;
-        if (arguments.length<=1) {
+    Function.prototype.bind = function(owner) {
+        var that = this;
+        if (arguments.length <= 1) {
             return function() {
                 return that.apply(owner, arguments);
             };
         } else {
-            var args= Array.prototype.slice.call(arguments, 1);
+            var args = Array.prototype.slice.call(arguments, 1);
             return function() {
-                return that.apply(owner, arguments.length===0? args : args.concat(Array.prototype.slice.call(arguments)));
+                return that.apply(owner, arguments.length === 0 ? args : args.concat(Array.prototype.slice.call(arguments)));
             };
         }
     };
 }
 
 if (!('format' in String.prototype)) {
-  String.prototype.format = function(owner) {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
+    String.prototype.format = function(owner) {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ?
+                args[number] :
+                match;
+        });
+    };
 }
 
 // Add ECMA262-5 string trim if not supported natively
 //
 if (!('trim' in String.prototype)) {
-    String.prototype.trim= function() {
+    String.prototype.trim = function() {
         return this.replace(/^\s+/, '').replace(/\s+$/, '');
     };
 }
 
 if (!('isNonEmpty' in String.prototype)) {
-    String.prototype.isNonEmpty= function() {
+    String.prototype.isNonEmpty = function() {
         return this !== null && this.trim() !== '' && this !== undefined;
     };
 }
@@ -117,87 +115,88 @@ if (!Array.prototype.contains) {
 }
 
 if (!Array.prototype.find) {
-  Array.prototype.find = function(predicate) {
-    if (this === null) {
-      throw new TypeError('Array.prototype.find called on null or undefined');
-    }
-    if (typeof predicate !== 'function') {
-      throw new TypeError('predicate must be a function');
-    }
-    var list = Object(this);
-    var length = list.length >>> 0;
-    var thisArg = arguments[1];
-    var value;
+    Array.prototype.find = function(predicate) {
+        if (this === null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
 
-    for (var i = 0; i < length; i++) {
-      value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
-      }
-    }
-    return undefined;
-  };
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
 }
 
 if (!('indexOf' in Array.prototype)) {
-    Array.prototype.indexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= 0;
-        if (i<0) i+= this.length;
-        if (i<0) i= 0;
-        for (var n= this.length; i<n; i++)
-            if (i in this && this[i]===find)
+    Array.prototype.indexOf = function(find, i /*opt*/ ) {
+        if (i === undefined) i = 0;
+        if (i < 0) i += this.length;
+        if (i < 0) i = 0;
+        for (var n = this.length; i < n; i++)
+            if (i in this && this[i] === find)
                 return i;
         return -1;
     };
 }
 if (!('lastIndexOf' in Array.prototype)) {
-    Array.prototype.lastIndexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= this.length-1;
-        if (i<0) i+= this.length;
-        if (i>this.length-1) i= this.length-1;
-        for (i++; i-->0;) /* i++ because from-argument is sadly inclusive */
-            if (i in this && this[i]===find)
+    Array.prototype.lastIndexOf = function(find, i /*opt*/ ) {
+        if (i === undefined) i = this.length - 1;
+        if (i < 0) i += this.length;
+        if (i > this.length - 1) i = this.length - 1;
+        for (i++; i-- > 0;) /* i++ because from-argument is sadly inclusive */
+            if (i in this && this[i] === find)
                 return i;
         return -1;
     };
 }
 if (!('forEach' in Array.prototype)) {
-    Array.prototype.forEach= function(action, that /*opt*/) {
-        var n=this.length;
-        for (var i= 0; i<n; i++)
+    Array.prototype.forEach = function(action, that /*opt*/ ) {
+        var n = this.length;
+        for (var i = 0; i < n; i++)
             if (i in this)
                 action.call(that, this[i], i, this);
     };
 }
 if (!('map' in Array.prototype)) {
-    Array.prototype.map= function(mapper, that /*opt*/) {
-        var other= new Array(this.length);
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.map = function(mapper, that /*opt*/ ) {
+        var other = new Array(this.length);
+        for (var i = 0, n = this.length; i < n; i++)
             if (i in this)
-                other[i]= mapper.call(that, this[i], i, this);
+                other[i] = mapper.call(that, this[i], i, this);
         return other;
     };
 }
 if (!('filter' in Array.prototype)) {
-    Array.prototype.filter= function(filter, that /*opt*/) {
-        var other= [], v;
-        for (var i=0, n= this.length; i<n; i++)
-            if (i in this && filter.call(that, v= this[i], i, this))
+    Array.prototype.filter = function(filter, that /*opt*/ ) {
+        var other = [],
+            v;
+        for (var i = 0, n = this.length; i < n; i++)
+            if (i in this && filter.call(that, v = this[i], i, this))
                 other.push(v);
         return other;
     };
 }
 if (!('every' in Array.prototype)) {
-    Array.prototype.every= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.every = function(tester, that /*opt*/ ) {
+        for (var i = 0, n = this.length; i < n; i++)
             if (i in this && !tester.call(that, this[i], i, this))
                 return false;
         return true;
     };
 }
 if (!('some' in Array.prototype)) {
-    Array.prototype.some= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.some = function(tester, that /*opt*/ ) {
+        for (var i = 0, n = this.length; i < n; i++)
             if (i in this && tester.call(that, this[i], i, this))
                 return true;
         return false;
@@ -205,7 +204,7 @@ if (!('some' in Array.prototype)) {
 }
 
 if (!('copyToClipboard' in String.prototype)) {
-    String.prototype.copyToClipboard= function() {
+    String.prototype.copyToClipboard = function() {
         // https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
         var textArea = document.createElement("textarea");
         textArea.style.position = 'fixed';
@@ -245,7 +244,7 @@ if (!String.prototype.includes) {
         if (typeof start !== 'number') {
             start = 0;
         }
-        
+
         if (start + search.length > this.length) {
             return false;
         } else {
@@ -255,130 +254,134 @@ if (!String.prototype.includes) {
 }
 
 if (!Array.prototype.includes) {
-  Object.defineProperty(Array.prototype, 'includes', {
-    value: function(searchElement, fromIndex) {
+    Object.defineProperty(Array.prototype, 'includes', {
+        value: function(searchElement, fromIndex) {
 
-      if (this == null) {
-        throw new TypeError('"this" is null or not defined');
-      }
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
 
-      // 1. Let O be ? ToObject(this value).
-      var o = Object(this);
+            // 1. Let O be ? ToObject(this value).
+            var o = Object(this);
 
-      // 2. Let len be ? ToLength(? Get(O, "length")).
-      var len = o.length >>> 0;
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
 
-      // 3. If len is 0, return false.
-      if (len === 0) {
-        return false;
-      }
+            // 3. If len is 0, return false.
+            if (len === 0) {
+                return false;
+            }
 
-      // 4. Let n be ? ToInteger(fromIndex).
-      //    (If fromIndex is undefined, this step produces the value 0.)
-      var n = fromIndex | 0;
+            // 4. Let n be ? ToInteger(fromIndex).
+            //    (If fromIndex is undefined, this step produces the value 0.)
+            var n = fromIndex | 0;
 
-      // 5. If n ≥ 0, then
-      //  a. Let k be n.
-      // 6. Else n < 0,
-      //  a. Let k be len + n.
-      //  b. If k < 0, let k be 0.
-      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+            // 5. If n ≥ 0, then
+            //  a. Let k be n.
+            // 6. Else n < 0,
+            //  a. Let k be len + n.
+            //  b. If k < 0, let k be 0.
+            var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
 
-      function sameValueZero(x, y) {
-        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
-      }
+            function sameValueZero(x, y) {
+                return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+            }
 
-      // 7. Repeat, while k < len
-      while (k < len) {
-        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
-        // b. If SameValueZero(searchElement, elementK) is true, return true.
-        if (sameValueZero(o[k], searchElement)) {
-          return true;
+            // 7. Repeat, while k < len
+            while (k < len) {
+                // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+                // b. If SameValueZero(searchElement, elementK) is true, return true.
+                if (sameValueZero(o[k], searchElement)) {
+                    return true;
+                }
+                // c. Increase k by 1. 
+                k++;
+            }
+
+            // 8. Return false
+            return false;
         }
-        // c. Increase k by 1. 
-        k++;
-      }
-
-      // 8. Return false
-      return false;
-    }
-  });
+    });
 }
 
 if (!Array.from) {
-  Array.from = (function () {
-    var toStr = Object.prototype.toString;
-    var isCallable = function (fn) {
-      return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
-    };
-    var toInteger = function (value) {
-      var number = Number(value);
-      if (isNaN(number)) { return 0; }
-      if (number === 0 || !isFinite(number)) { return number; }
-      return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
-    };
-    var maxSafeInteger = Math.pow(2, 53) - 1;
-    var toLength = function (value) {
-      var len = toInteger(value);
-      return Math.min(Math.max(len, 0), maxSafeInteger);
-    };
+    Array.from = (function() {
+        var toStr = Object.prototype.toString;
+        var isCallable = function(fn) {
+            return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+        };
+        var toInteger = function(value) {
+            var number = Number(value);
+            if (isNaN(number)) {
+                return 0;
+            }
+            if (number === 0 || !isFinite(number)) {
+                return number;
+            }
+            return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+        };
+        var maxSafeInteger = Math.pow(2, 53) - 1;
+        var toLength = function(value) {
+            var len = toInteger(value);
+            return Math.min(Math.max(len, 0), maxSafeInteger);
+        };
 
-    // The length property of the from method is 1.
-    return function from(arrayLike/*, mapFn, thisArg */) {
-      // 1. Let C be the this value.
-      var C = this;
+        // The length property of the from method is 1.
+        return function from(arrayLike /*, mapFn, thisArg */ ) {
+            // 1. Let C be the this value.
+            var C = this;
 
-      // 2. Let items be ToObject(arrayLike).
-      var items = Object(arrayLike);
+            // 2. Let items be ToObject(arrayLike).
+            var items = Object(arrayLike);
 
-      // 3. ReturnIfAbrupt(items).
-      if (arrayLike == null) {
-        throw new TypeError('Array.from requires an array-like object - not null or undefined');
-      }
+            // 3. ReturnIfAbrupt(items).
+            if (arrayLike == null) {
+                throw new TypeError('Array.from requires an array-like object - not null or undefined');
+            }
 
-      // 4. If mapfn is undefined, then let mapping be false.
-      var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
-      var T;
-      if (typeof mapFn !== 'undefined') {
-        // 5. else
-        // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
-        if (!isCallable(mapFn)) {
-          throw new TypeError('Array.from: when provided, the second argument must be a function');
-        }
+            // 4. If mapfn is undefined, then let mapping be false.
+            var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+            var T;
+            if (typeof mapFn !== 'undefined') {
+                // 5. else
+                // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+                if (!isCallable(mapFn)) {
+                    throw new TypeError('Array.from: when provided, the second argument must be a function');
+                }
 
-        // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
-        if (arguments.length > 2) {
-          T = arguments[2];
-        }
-      }
+                // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+                if (arguments.length > 2) {
+                    T = arguments[2];
+                }
+            }
 
-      // 10. Let lenValue be Get(items, "length").
-      // 11. Let len be ToLength(lenValue).
-      var len = toLength(items.length);
+            // 10. Let lenValue be Get(items, "length").
+            // 11. Let len be ToLength(lenValue).
+            var len = toLength(items.length);
 
-      // 13. If IsConstructor(C) is true, then
-      // 13. a. Let A be the result of calling the [[Construct]] internal method 
-      // of C with an argument list containing the single item len.
-      // 14. a. Else, Let A be ArrayCreate(len).
-      var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+            // 13. If IsConstructor(C) is true, then
+            // 13. a. Let A be the result of calling the [[Construct]] internal method 
+            // of C with an argument list containing the single item len.
+            // 14. a. Else, Let A be ArrayCreate(len).
+            var A = isCallable(C) ? Object(new C(len)) : new Array(len);
 
-      // 16. Let k be 0.
-      var k = 0;
-      // 17. Repeat, while k < len… (also steps a - h)
-      var kValue;
-      while (k < len) {
-        kValue = items[k];
-        if (mapFn) {
-          A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
-        } else {
-          A[k] = kValue;
-        }
-        k += 1;
-      }
-      // 18. Let putStatus be Put(A, "length", len, true).
-      A.length = len;
-      // 20. Return A.
-      return A;
-    };
-  }());
+            // 16. Let k be 0.
+            var k = 0;
+            // 17. Repeat, while k < len… (also steps a - h)
+            var kValue;
+            while (k < len) {
+                kValue = items[k];
+                if (mapFn) {
+                    A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+                } else {
+                    A[k] = kValue;
+                }
+                k += 1;
+            }
+            // 18. Let putStatus be Put(A, "length", len, true).
+            A.length = len;
+            // 20. Return A.
+            return A;
+        };
+    }());
 }
